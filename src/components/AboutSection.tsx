c30fa6d8b45e7cb4e652d987a06f132d093dde9c
S. D. Maat <silvijndemaat1@gmail.com>
2026-06-13 18:01:47 +0200

    text

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -2,7 +2,10 @@ export default function AboutSection() {
   return (
     <section id="about" className="about">
       <h2>Computers have always been the most interesting things to me.</h2>
-        <p>Learning to understand them and create things with them has been a wonderful journey that I'd like to share here. I started programming in 2020, since then I have become experienced in various programming languages and frameworks, and I'm always eager to learn more and take on new challenges.</p>
+        <p>Learning to understand them and create things with them has been a wonderful journey that I'd like to share here.</p>
+        <p>I started programming in 2020, a tough year to start anything, I feel like it made me an more independent developer and person in general.</p>
+        <p>In my free time I enjoy listening to music, working out or watching a good movie.</p>
+        <p>I would love for you to check out my projects! Have a nice day!</p>
     </section>
   );
 }
